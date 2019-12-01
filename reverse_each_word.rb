6bc46a7508af8631d3f6c351de5091122fa39e75
Erik Kasperson <chefkasperson@gmail.com>
2019-12-01 07:32:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,11 @@ def reverse_each_word(phrase)
   y = phrase.split(" ")
   z = []
   i = 0
-  while i < y.length do 
-    x = y[i].reverse
-    z << x
-  i += 1 
-  end
+  # while i < y.length do 
+  #   x = y[i].reverse
+  #   z << x
+  # i += 1 
+  # end
   q = z.join(" ")
   puts q
   q
